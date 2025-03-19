ad8c85c53318dd2e2d26e759b56ddf4187faf8c8
doctormatterhorn <dmf030@gmail.com>
2025-03-18 21:42:23 -0300

    fixed a mistake in test

--- tests.py
+++ tests.py
@@ -2,10 +2,10 @@ import os
 import pytest
 import tempfile
 
-from main import copy_file
+from main import copy_files
 
 
-def test_copy_file():
+def test_copy_files():
     with tempfile.TemporaryDirectory() as source, tempfile.TemporaryDirectory() as replica:
         source_file_path = source + '/test.txt'
         replica_file_path = replica + '/test.txt'
@@ -13,7 +13,7 @@ def test_copy_file():
         with open(source_file_path, 'w') as f:
             f.write('test')
         
-        copy_file(source_file_path, replica)
+        copy_files(source, replica)
 
         assert os.path.exists(replica_file_path)
         with open(replica_file_path, 'r') as f:
