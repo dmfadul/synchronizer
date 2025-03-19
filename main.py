e6f2e3edbdccd564a4536b798ac967a0a118fa3a
doctormatterhorn <dmf030@gmail.com>
2025-03-18 21:42:38 -0300

    added logic to copy_files

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
-def copy_file():
-    pass
+import os
+import shutil
+
+SOURCE = 'source'
+REPLICA = 'replica'
+
+
+def copy_files(path_to_source, path_to_replica):
+    for root, dir, files in os.walk(path_to_source):
+        folder_in_source = os.path.relpath(root, path_to_source)
+        folder_in_replica = os.path.join(path_to_replica, folder_in_source)
+
+        if not os.path.exists(folder_in_replica):
+            os.makedirs(folder_in_replica)
+
+        for file in files:
+            source_file = os.path.join(root, file)
+            replica_file = os.path.join(folder_in_replica, file)
+
+            if not os.path.exists(replica_file):
+                shutil.copy2(source_file, replica_file)
+                
+
+if __name__ == '__main__':
+    copy_files(SOURCE, REPLICA)
